c2315376dd594fae339d2db996eaf32bd93ca677
kr-kashyap <kashyap.n@knackroot.com>
2025-01-28 19:31:25 +0530

    Add a comment related to existing delay for cloud backup.

--- backup/cloudBackup.ts
+++ backup/cloudBackup.ts
@@ -84,6 +84,7 @@ async function runBackupAndUpload() {
 cron.schedule(`0 */${paymentInterval} * * *`, async () => {
     console.log(`Scheduled backup and upload started at ${new Date().toISOString()}`);
 
+    // We are waiting for a delay after the payment cycle, before pushing the DB dump to Google drive.
     const delay = (10 * 60 * 1000); // Convert to milliseconds
 
     // Wait for the delay before executing the backup and upload
