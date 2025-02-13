9519ed175ec8241cccff8069e89623f3f965d7eb
kr-kashyap <kashyap.n@knackroot.com>
2025-02-13 17:02:40 +0530

    Add nacho_payments table.

--- init.sql
+++ init.sql
@@ -19,6 +19,14 @@ CREATE TABLE IF NOT EXISTS payments (
     transaction_hash VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS nacho_payments (
+    id SERIAL PRIMARY KEY,
+    wallet_address TEXT[] NOT NULL,
+    nacho_amount BIGINT NOT NULL,
+    timestamp TIMESTAMP DEFAULT NOW(),
+    transaction_hash VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS block_details (
     mined_block_hash VARCHAR(255) PRIMARY KEY,
     miner_id VARCHAR(255),
--- migration.sql
+++ migration.sql
@@ -1,1 +1,9 @@
 ALTER TABLE miners_balance ADD nacho_rebate_kas NUMERIC DEFAULT 0;
+
+CREATE TABLE IF NOT EXISTS nacho_payments (
+    id SERIAL PRIMARY KEY,
+    wallet_address TEXT[] NOT NULL,
+    nacho_amount BIGINT NOT NULL,
+    timestamp TIMESTAMP DEFAULT NOW(),
+    transaction_hash VARCHAR(255) NOT NULL
+);
