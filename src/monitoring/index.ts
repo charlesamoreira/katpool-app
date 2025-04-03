1f0e5fc089952ad44d5ea590ef868e6112df7beb
kr-kashyap <kashyap.n@knackroot.com>
2025-04-03 18:49:57 +0530

    Add file based logging.

--- src/monitoring/index.ts
+++ src/monitoring/index.ts
@@ -1,22 +1,60 @@
-import { stylize, codes, getReadableDate, getReadableTime } from './styling';
+import { getReadableDate, getReadableTime } from './styling';
 import PQueue from 'p-queue';
+import winston from 'winston';
+import 'winston-daily-rotate-file';
+import chalk from 'chalk';
 
 interface LogJobData {
-  level: 'LOG' | 'DEBUG' | 'ERROR';
+  level: 'DEBUG' | 'ERROR' | 'INFO';
   message: string;
 }
+
+const { combine } = winston.format;
 
 export default class Monitoring {
   private logQueue: PQueue;
   private debugEnabled: boolean;
+  private logger: winston.Logger;
 
-  constructor() {
+  private fileNameFormat = `app-%DATE%.log`;
+  constructor(logFilePath: string = process.env.LOG_FILE_PATH+this.fileNameFormat || `logs/${this.fileNameFormat}`) {
+
+    const logFormat = winston.format.printf((info) => {
+      const levelColor = {
+        info: chalk.bgYellowBright.whiteBright,
+        error: chalk.bgYellowBright.whiteBright,
+        debug: chalk.bgYellowBright.whiteBright,
+        warn: chalk.bgYellowBright.whiteBright
+      }[info.level] || chalk.whiteBright; 
+    
+      return `${chalk.green(getReadableDate())} ${chalk.cyan(getReadableTime())} ${levelColor(info.level.toUpperCase())}: ${chalk.whiteBright(info.message)}`;
+    });
+
     this.logQueue = new PQueue({ concurrency: 1 });
-    this.debugEnabled = process.env.DEBUG === '1';
+    this.debugEnabled = process.env.DEBUG?.trim() === '1';
+
+    let logLevel = 'info';
+    if (this.debugEnabled) logLevel = 'debug' 
+    this.logger = winston.createLogger({
+      level: logLevel,  
+      format: combine(logFormat),
+      transports: [
+        new winston.transports.DailyRotateFile({
+          filename: logFilePath,
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: true,
+          maxSize: '1k',      
+          maxFiles: '14d',    
+        }),
+        new winston.transports.Console({
+          format: combine(logFormat),
+        }),
+      ],
+    });
   }
 
   log(message: string) {
-    this.logQueue.add(() => this.processLog({ level: 'LOG', message }));
+    this.logQueue.add(() => this.processLog({ level: 'INFO', message }));
   }
 
   debug(message: string) {
@@ -31,12 +69,7 @@ export default class Monitoring {
 
   private async processLog(job: LogJobData) {
     const { level, message } = job;
-    const prefix = stylize(codes.bgYellowLight, level);
-    console.log(this.buildMessage(prefix, message));
-  }
-
-  private buildMessage(prefix: string, message: string) {
-    return `${stylize(codes.green, getReadableDate())} ${stylize(codes.cyan, getReadableTime())} ${prefix} ${message}`;
+    this.logger.log(level.toLowerCase(), message);
   }
 
   async waitForQueueToDrain() {
